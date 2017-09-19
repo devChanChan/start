927047af3d8a9e60b5862383f10b475ee5fbe2f1
kimseungchan <withksc@gmail.com>
2017-09-19 19:17:06 -0400

    my_first_java

---
+++ history.java
@@ -0,0 +1,12 @@
+public class Variables {
+    
+    public static void main(String[] args) {
+        
+        // find out variables that are valid
+        int Xpoint = 13;
+        
+        //display the decalred variable 
+        System.out.println(Xpoint);
+    }
+}
+               
