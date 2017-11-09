89049c32aa4baa8465d0759fdf054e85e23fd3af
SeungChan Kim <withksc@gmail.com>
2017-11-08 21:19:10 -0500

    Add files via upload
    My First Java assignment

---
+++ Lab1.java
@@ -0,0 +1,45 @@
+/*
+ * Name:  Seungchan Kim
+ * Assignment:  Lab 1
+ * Program: Computer Systems Network
+ * 
+ * Followings are the information about myself and the answer to the bonus quiz.
+ */
+
+public class Lab1 {
+    
+    public static void main(String[] args) {
+        
+        // display information about myself
+        System.out.println("Name: Seungchan Kim");
+        System.out.println("Programming Experience: none, but I like to sit on"
+            + " the keyboard and make organized documents using computer.");
+        System.out.println("Favourite Subject: International Relations, Math");
+        System.out.println("Least Favourite Subject: Poet");
+        System.out.println("Interests: hiking, riding a bicycle, swimming,"
+            + " watching drama, work out, eating.");
+        System.out.println("I have spent two years trading edible oils&fats "
+            + "in Korea before. I used to import GMO Canola oil from Canada "
+            + "and I have never imagined to come to Canada just a year ago. "
+            + "But here I am. I have wanted to work in more free environment "
+            + "with the computer programming skills and I am looking forward "
+            + "to learning it.\n");    
+
+        // display answers to the bonus quiz
+        System.out.println("Why do we document our code?: For even programmers,"
+            + " it could be difficult to read codes that the others made. "
+            + "To make it easy, comments can help them to understand what for "
+            + "and why statements are written down. So, documenting can help "
+            + "programmers to communicate with the program much easily.");
+        System.out.println("What is a syntax error?: A syntax error comes out "
+            + "when you compile a source code if a sequence of characters is "
+            + "wrong or some characters are missing. It is an error in the "
+            + "syntax of programming language.");
+        System.out.println("What's the difference between System.out.print() "
+            + "and System.out.println()?: System.out.print() is to print your "
+            + "stuff and does not add a new line. On the other hand, System."
+            + "out.println() is to print your stuff and add a new line in the "
+            + "end.");
+                    
+    }
+}
